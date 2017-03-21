f6808dc1032eb4e204a4c6910b46d5f08931f13c
Randy Sargent <randy.sargent@gmail.com>
2017-03-21 14:46:35 -0400

    emails if running out of pix

--- MagicPictureFrame3.py
+++ MagicPictureFrame3.py
@@ -1,8 +1,17 @@
 from google.appengine.ext import vendor
+from google.appengine.api import mail
 vendor.add('lib')
 
 import datetime, json, pytz, urllib2, webapp2
 from google.appengine.api import users
+
+startDate = datetime.date(2016, 8, 9)
+username = 'brian.sargent.nj'
+albumId = '6166497731584716881'
+authKey = 'Gv1sRgCIeFvff2vZ3hRw'
+
+#albumId = '5661873495373261201'
+#authKey = 'Gv1sRgCP729Mbsh7XdNg'
 
 def getPhotos(username, albumid, authkey):
     startIndex = 1
@@ -37,25 +46,53 @@ def getPhotos(username, albumid, authkey):
 
 
 def endingToday(username, albumId, authKey):
-    startDate = datetime.date(2013, 2, 14)
     now = datetime.datetime.now(pytz.timezone('US/Eastern')).date()
     ndays = (now - startDate).days
 
     photos = getPhotos(username, albumId, authKey)
+    number_of_days_left = len(photos) - (ndays + 1)
+    if number_of_days_left <= 10:
+        mail.send_mail(sender='Randy Sargent <randy.sargent@gmail.com>',
+                   to='Randy Sargent <randy.sargent@gmail.com>, Brian Sargent <brian.sargent.nj@gmail.com>',
+                   subject="Magic Picture Frame down to %d pictures" % number_of_days_left,
+                   body="""Dear Brian and Randy,
+
+Someone just visited the Magic Picture Frame and I noticed you only have %d pictures left...
+
+Love,
+The Magic Picture Frame Server
+""" % number_of_days_left )
+
     return list(reversed(photos[: ndays + 1]))
+
+def startingToday(username, albumId, authKey):
+    now = datetime.datetime.now(pytz.timezone('US/Eastern')).date()
+    ndays = (now - startDate).days
+
+    photos = getPhotos(username, albumId, authKey)
+    return list(photos[ndays :])
 
 class EndingTodayJson(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'application/json'
         self.response.headers.add_header('Access-Control-Allow-Origin', '*')
 
-        username = 'brian.sargent.nj'
-        albumId = '5661873495373261201'
-        authKey = 'Gv1sRgCP729Mbsh7XdNg'
 
         self.response.write(json.dumps(endingToday(username, albumId, authKey)))
 
+class Peek(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/html'
+
+        self.response.write('<html><head></head><body>Peek:<br>\n')
+        day = datetime.datetime.now(pytz.timezone('US/Eastern')).date()
+        for photo in startingToday(username, albumId, authKey):
+            self.response.write('%s: <img width=100 src="%s"><br>' % (day, photo))
+            day += datetime.timedelta(days=1)
+        self.response.write('</body></html>\n')
+
 app = webapp2.WSGIApplication([
-    ('/endingToday.json', EndingTodayJson)
+    ('/endingToday.json', EndingTodayJson),
+    ('/peek', Peek)
 ], debug=True)
 
